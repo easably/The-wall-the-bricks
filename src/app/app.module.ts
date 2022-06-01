31f4f4c1f9c2bc0d904a55ba27931c43272818bd
mac <darya0revtovich@gmail.com>
2022-06-01 17:23:03 +0300

    diplom1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,27 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, provideFirebaseApp(() => initializeApp({    apiKey: 'AIzaSyD31jrwswM-xKIJk7fCeuRmt2GEBKr7Xnw',
+    authDomain: "trainer-3c458.firebaseapp.com",
+    databaseURL: "https://trainer-3c458-default-rtdb.firebaseio.com",
+    projectId: "trainer-3c458",
+    storageBucket: "trainer-3c458.appspot.com",
+    messagingSenderId: "903016268032",
+    appId: "1:903016268032:web:77c6a26ec76253ea10b3e5",
+    measurementId: "G-TZSV5FDG1M", })),
+    provideFirestore(() => getFirestore()),],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+
+
